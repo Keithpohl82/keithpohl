e887953367f5fb08e11ca5e54606b9cbcc63bd46
Keith Pohl <49458505+Keithpohl82@users.noreply.github.com>
2019-05-07 06:02:24 -0500

    added chat bot

---
+++ chatbot.js
@@ -0,0 +1,9 @@
+const  AssistantV1 = require('ibm-watson/assistant/v1');
+
+const assistant = new AssistantV1({
+  version: '2019-02-28',
+  username: 'jluLyEEeCJsK5geoYDrtcKRIuOf8YTHv94KxrH9ApkPy',
+  password: 'jluLyEEeCJsK5geoYDrtcKRIuOf8YTHv94KxrH9ApkPy',
+  url: 'https://gateway.watsonplatform.net/assistant/api'
+});
+
